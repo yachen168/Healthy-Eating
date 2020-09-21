4d8c9e58e89bc1d74fd47a20f64b1985f0beb820
yachen <io260188@gmail.com>
2020-09-22 00:49:22 +0800

    feat: BaseTitle

---
+++ src/components/common/BaseTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1 class="page-title">
+    {{ title }}
+  </h1>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.page-title {
+  margin-bottom: 60px;
+  font-size: 24px;
+  color: rgb(79, 123, 98);
+}
+</style>
