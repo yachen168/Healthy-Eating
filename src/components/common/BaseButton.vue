ae3757b0d288c62c190e4a908f077f4269b537d5
yachen <io260188@gmail.com>
2020-09-22 00:48:39 +0800

    feat: BaseButton

---
+++ src/components/common/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="button" :class="buttonClass" @click="$emit('clickHandler')">
+    {{ buttonText }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      required: true
+    },
+    buttonClass: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.button {
+  padding: 10px 30px;
+  border-radius: 5px;
+  font-weight: 500;
+  text-align: center;
+  cursor: pointer;
+}
+
+.button.primary {
+  color: #fff;
+  background-color: rgb(79, 123, 98);
+}
+
+.button.default {
+  color: rgb(79, 123, 98);
+  background-color: #fff;
+}
+</style>
