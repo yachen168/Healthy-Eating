6699e2fd6835ac9389c502643ca17c0c3f2f81ea
yachen <io260188@gmail.com>
2020-09-22 00:49:57 +0800

    feat: common navbar

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="header">
+    <div>
+      <slot name="left"></slot>
+    </div>
+    <div>
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  background-color: rgb(79, 123, 98);
+  .logo {
+    font-size: 24px;
+    letter-spacing: 1px;
+    color: #fff;
+    cursor: pointer;
+  }
+  .button {
+    display: inline-block;
+    font-weight: 400;
+    cursor: pointer;
+  }
+  .button-login {
+    color: #fff;
+    margin-right: 10px;
+  }
+}
+</style>
