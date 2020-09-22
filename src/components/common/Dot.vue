34521eb8b48892df18282043598b8dc0506c98b8
yachen <io260188@gmail.com>
2020-09-23 02:27:12 +0800

    feat: RecordingRow component

---
+++ src/components/common/Dot.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 之後可刪除，設計師會出圖 -->
+  <div class="dot" :class="size"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.dot {
+  border-radius: 50%;
+  background-color: #c4c4c4;
+}
+
+.dot.sm {
+  width: 30px;
+  height: 30px;
+}
+
+.dot.md {
+  width: 40px;
+  height: 40px;
+}
+.dot.lg {
+  width: 48px;
+  height: 48px;
+}
+</style>
