1eee0c1cfe0d98f8433edcfe54d0648490d15b88
Larry <shaka422@hotmail.com>
2020-09-25 16:17:24 +0800

    feat:共用按鈕完成

---
+++ src/components/common/commcoButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="commcoButton">
+    <b-button
+      block
+      class="base-button"
+      :disabled="disabledState"
+      :class="{ 'disable-button': disabledState }"
+      @click="$emit('update:check')"
+      >{{ title }}</b-button
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "commcoButton",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    disabledState: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.disable-button.btn-secondary.disabled {
+  background-color: #a8a8a8;
+  border-color: #a8a8a8;
+}
+
+.btn-secondary:hover {
+  background-color: #407d60;
+  border-color: #407d60;
+}
+
+.base-button {
+  // width: 296px;
+  height: 40px;
+  font-size: 14px;
+  font-family: Roboto;
+  font-weight: 700;
+  border-radius: 6px;
+  background-color: #407d60;
+  border-color: #407d60;
+  margin: 0 auto;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,49 @@
 <template>
   <div class="login-page">
-    <Navbar> </Navbar>
-    <BaseTitle title="登入"></BaseTitle>
-    <BaseButton buttonText="登入" buttonClass="disabled"></BaseButton>
+    <Navbar></Navbar>
+    <section class="main">
+      <label :for="test"
+        >Type <code>{{ type }}</code
+        >:</label
+      >
+      <b-form-input :id="test" :type="type"></b-form-input>
+      <commcoButton title="登入" :disabledState="state"></commcoButton>
+    </section>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/common/Navbar";
-import BaseTitle from "@/components/common/BaseTitle";
-import BaseButton from "@/components/common/BaseButton";
-
+import commcoButton from "@/components/common/commcoButton";
 export default {
+  data() {
+    return {
+      state: true
+    };
+  },
+  methods: {},
   components: {
     Navbar,
-    BaseTitle,
-    BaseButton
+    commcoButton
   }
 };
 </script>
 
 <style lang="scss" scoped>
-::v-deep .ant-form-item-label {
-  padding: 0;
-  line-height: 1.5;
-  label {
-    font-size: 16px;
-    font-weight: 500;
-    color: #4f7b62;
-  }
+::v-deep .main {
+  padding: 0 32px;
 }
+// ::v-deep .ant-form-item-label {
+//   padding: 0;
+//   line-height: 1.5;
+//   label {
+//     font-size: 16px;
+//     font-weight: 500;
+//     color: #4f7b62;
+//   }
+// }
 
-::v-deep .ant-input {
-  border-color: #4f7b62;
-}
+// ::v-deep .ant-input {
+//   border-color: #4f7b62;
+// }
 </style>
