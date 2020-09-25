c657647cd9ae220741cdf04918b61d2a0a4d4023
Larry <shaka422@hotmail.com>
2020-09-25 20:35:25 +0800

    feate commonInput新增密碼眼睛

--- src/plugins/bootstrap.js
+++ src/plugins/bootstrap.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
-import { ButtonPlugin, FormInputPlugin } from "bootstrap-vue";
+import {
+  ButtonPlugin,
+  FormInputPlugin,
+  BootstrapVueIcons
+} from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-vue/dist/bootstrap-vue-icons.min.css";
+
+Vue.use(BootstrapVueIcons);
 Vue.use(ButtonPlugin);
 Vue.use(FormInputPlugin);
