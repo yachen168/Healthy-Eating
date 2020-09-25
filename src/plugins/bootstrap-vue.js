2b2e087e8f9caa69898d1e2d15aa0b717c79438a
yachen <io260188@gmail.com>
2020-09-25 21:25:38 +0800

    update: bootrap-vue plugin

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,8 +1,14 @@
 import Vue from "vue";
-import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
-
+import {
+  ButtonPlugin,
+  FormInputPlugin,
+  TablePlugin,
+  BootstrapVueIcons
+} from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-Vue.use(BootstrapVue);
+Vue.use(ButtonPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(TablePlugin);
 Vue.use(BootstrapVueIcons);
