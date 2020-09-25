33371105ce8ccb6dbbb46ac1864a19ecabb9c67f
Larry <shaka422@hotmail.com>
2020-09-25 17:32:52 +0800

    feat:commonInput元件完成

--- src/components/common/commonInput.vue
+++ src/components/common/commonInput.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <label :for="inputId" class="label-title">{{ title }}</label>
+    <b-form-input
+      :id="inputId"
+      :placeholder="placeholder"
+      class="form-input"
+      @input="$emit('update:input', $event)"
+    ></b-form-input>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+  name: "commonInput",
+  props: {
+    inputId: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      required: true
     }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.label-title {
+  color: #407d60;
+  line-height: 2.2;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 13px;
+  margin-top: 3px;
+  margin-bottom: 0;
+}
 
+.form-input {
+  border-radius: 6px;
+}
+
+::placeholder {
+  color: #a8a8a8;
+}
 </style>
