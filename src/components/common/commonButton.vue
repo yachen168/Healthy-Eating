4115d69700eed102551fa805d3b91f08736095d5
yachen <io260188@gmail.com>
2020-09-27 13:59:24 +0800

    feat: props style class to commonButton

--- src/components/common/commonButton.vue
+++ src/components/common/commonButton.vue
@@ -4,7 +4,7 @@
       block
       class="base-button"
       :disabled="disabledState"
-      :class="{ 'disable-button': disabledState }"
+      :class="[{ 'disable-button': disabledState }, buttonStyle]"
       @click="$emit('update:check')"
       >{{ title }}</b-button
     >
@@ -22,6 +22,10 @@ export default {
     disabledState: {
       type: Boolean,
       required: true
+    },
+    buttonStyle: {
+      type: String,
+      default: ""
     }
   }
 };
@@ -47,5 +51,40 @@ export default {
   background-color: #407d60;
   border-color: #407d60;
   margin: 0 auto;
+}
+
+/* primary: 白字綠底 */
+.base-button.primary {
+  color: #fff;
+  background-color: #407d60;
+}
+
+/* default: 綠字白底 */
+.base-button.default {
+  color: #407d60;
+  background-color: #fff;
+}
+
+/* info: 白字藍底 */
+.base-button.info {
+  color: #fff;
+  background-color: #4b7cc7;
+}
+
+/* disabled: 白字灰底 */
+.base-button.disabled {
+  color: #fff;
+  background-color: #a8a8a8;
+}
+
+/* danger: 白字紅底 */
+.base-button.danger {
+  color: #fff;
+  background-color: #e36e6e;
+}
+
+/* danger: 白字透明底 */
+.base-button.transparent {
+  color: #fff;
 }
 </style>
