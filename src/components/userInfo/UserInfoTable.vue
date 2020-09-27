b8a669ac57f8f2697a557ebd45ddf38ed0bca27f
yachen <io260188@gmail.com>
2020-09-27 16:07:02 +0800

    feat: defualt values for userinfo table

--- src/components/userInfo/UserInfoTable.vue
+++ src/components/userInfo/UserInfoTable.vue
@@ -6,17 +6,20 @@
         <span v-if="data.value === 'female'">女</span>
         <span v-if="data.value === 'male'">男</span>
         <span v-if="data.value === 'others'">其他</span>
+        <span v-else class="empty">未選擇</span>
       </template>
 
       <!-- ===== 轉換生日欄的資料格式 ======= -->
       <template v-slot:cell(birthday)="data">
-        <span>{{ data.value.split("-").join("/") }}</span>
+        <span v-if="data.value">{{ data.value.split("-").join("/") }}</span>
+        <span v-else class="empty">年/月/日</span>
       </template>
 
       <!-- ===== 身高欄的單位顏色、間距 ======= -->
       <template v-slot:cell(height)="data">
         <span>{{ data.value }}</span>
-        <span class="unit unit-height">公分/cm</span>
+        <span v-if="data.value" class="unit unit-height">公分/cm</span>
+        <span v-else class="empty">未填寫</span>
       </template>
 
       <!-- ===== 體重欄的單位顏色、間距 ======= -->
@@ -64,6 +67,12 @@ export default {
   &-weight {
     margin-left: 20px;
   }
+}
+
+.empty {
+  font-size: 14px;
+  font-weight: 700;
+  color: #a8a8a8;
 }
 
 ::v-deep .table td {
