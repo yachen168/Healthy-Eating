e7b1d13a85bbde613c888401e3f1a781d27e1bb9
yachen <io260188@gmail.com>
2020-09-29 00:46:07 +0800

    feat: sticky navbar

--- src/components/common/Navbar.vue
+++ src/components/common/Navbar.vue
@@ -18,9 +18,12 @@ export default {};
   display: flex;
   justify-content: space-between;
   align-items: center;
+  position: sticky;
+  top: 0;
   padding: 0 16px;
   height: 64px;
   background-color: #407d60;
+  z-index: 10;
   .logo {
     font-size: 24px;
     letter-spacing: 1px;
