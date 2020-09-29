5b051d9cb2ecc2c05f2239d8037b10f6bde2ed57
Larry <shaka422@hotmail.com>
2020-09-29 20:30:47 +0800

    feat member-application 巢狀路由:登入、註冊路徑設定

---
+++ src/views/member/MemberApplication.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="member-application">
+    <Navbar></Navbar>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/common/Navbar";
+export default {
+  name: "MemberApplication",
+  components: {
+    Navbar
+    // PasswordInput
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
