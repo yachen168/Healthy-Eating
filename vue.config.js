117dc4ddbffdcb0aeb4b3031619c3d3023ceccfd
yachen <io260188@gmail.com>
2020-10-01 17:06:54 +0800

    chore: settings fot using both inline and external SVGs

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,20 @@ module.exports = {
     svgRule.uses.clear();
 
     svgRule
+      .oneOf("inline")
+      .resourceQuery(/inline/)
       .use("babel-loader")
       .loader("babel-loader")
       .end()
       .use("vue-svg-loader")
-      .loader("vue-svg-loader");
+      .loader("vue-svg-loader")
+      .end()
+      .end()
+      .oneOf("external")
+      .use("file-loader")
+      .loader("file-loader")
+      .options({
+        name: "assets/[name].[hash:8].[ext]"
+      });
   }
 };
