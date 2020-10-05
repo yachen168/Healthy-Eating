3829c134de6f84e2554e99d1db36701a8c56ba6d
yachen <io260188@gmail.com>
2020-10-05 22:56:22 +0800

    feat: chartNav

---
+++ src/views/charts/ChartEntry.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ChartNav></ChartNav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import ChartNav from "@/components/charts/ChartNav";
+
+export default {
+  components: {
+    ChartNav
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
