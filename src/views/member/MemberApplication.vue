68937d6a65928cf9c64a1b06610a854f014b9099
xxxxtim <xxxxtim@hotmail.com>
2020-10-05 17:04:16 +0800

    fix: coding style

--- src/views/member/MemberApplication.vue
+++ src/views/member/MemberApplication.vue
@@ -1,18 +1,18 @@
 <template>
-<div class="member-application">
+  <div class="member-application">
     <Navbar></Navbar>
     <router-view />
-</div>
+  </div>
 </template>
 
 <script>
 import Navbar from "@/components/common/Navbar";
 export default {
-    name: "MemberApplication",
-    components: {
-        Navbar
-        // PasswordInput
-    }
+  name: "MemberApplication",
+  components: {
+    Navbar
+    // PasswordInput
+  }
 };
 </script>
 
