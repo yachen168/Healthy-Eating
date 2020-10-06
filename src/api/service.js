7d5d148559bbdd9fc2067b0e0bb98bfac244d219
Larry <shaka422@hotmail.com>
2020-10-06 16:52:10 +0800

    feat 攔截器 ＆ 登入頁面串接完成

--- src/api/service.js
+++ src/api/service.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import router from "../router";
+
 const domainURL = "https://k88d02.ml";
 
 const API = axios.create({
@@ -8,5 +10,33 @@ const API = axios.create({
     Accept: "application/json"
   }
 });
+
+API.interceptors.request.use(
+  function(config) {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = token;
+    }
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
+
+API.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  function(error) {
+    if (
+      error.response.status === 400 &&
+      router.history.current.name !== "Login"
+    ) {
+      router.push({ name: "Login" });
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default API;
