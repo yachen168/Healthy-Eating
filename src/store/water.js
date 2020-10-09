d41c8457e689954d311080f4f6357f967c538b86
yachen <io260188@gmail.com>
2020-10-10 03:47:44 +0800

    feat: record water intake

---
+++ src/store/water.js
@@ -0,0 +1,36 @@
+import API from "../api/service";
+export default {
+  state: {
+    sumWaterIntake: []
+  },
+  mutations: {
+    sumWaterIntake(state, sumWaterIntake) {
+      state.sumWaterIntake = sumWaterIntake;
+    }
+  },
+  actions: {
+    async fetchSumWaterIntake({ commit }, data) {
+      try {
+        const response = await API.post("/water/sum", data);
+        commit("sumWaterIntake", response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async updateSumWaterIntake({}, data) {
+      try {
+        const response = await API.post("/userWater", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  },
+  getters: {
+    sumWaterIntake(state) {
+      return state.sumWaterIntake;
+    },
+    sumWaterIntakeOneDay(state) {
+      return state.sumWaterIntake.length ? +state.sumWaterIntake[0].sum : 0;
+    }
+  }
+};
