72eb8c1f3fda8877a35de0d43d41cbb9a10b1e93
xxxxtim <xxxxtim@hotmail.com>
2020-10-13 15:46:21 +0800

    feat: password vuex setting

---
+++ src/store/editPassword.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import API from "../api/service";
+import router from "../router";
+Vue.use(Vuex);
+export default new Vuex.Store({
+  state: {},
+  mutations: {},
+  actions: {},
+  getters: {},
+  modules: {}
+});
