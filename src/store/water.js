9205a4907130a45046e5bc9d6cb045ac1ed96fcb
yachen <io260188@gmail.com>
2020-10-17 14:13:44 +0800

    refactor: 飲水紀錄資料流

--- src/store/water.js
+++ src/store/water.js
@@ -1,11 +1,31 @@
 import API from "../api/service";
 export default {
   state: {
-    sumWaterIntake: []
+    sumWaterIntake: [],
+    idOfWaterIntake: [],
+    sumWaterIntakeOneDay: 0,
+    initSumWaterIntakeOneDay: 0
   },
   mutations: {
     sumWaterIntake(state, sumWaterIntake) {
       state.sumWaterIntake = sumWaterIntake;
+    },
+    initSumWaterIntakeOneDay(state) {
+      if (state.sumWaterIntake.length) {
+        state.sumWaterIntakeOneDay = +state.sumWaterIntake[0].sum;
+      } else {
+        state.sumWaterIntakeOneDay = 0;
+      }
+    },
+    idOfWaterIntake(state, { data, date }) {
+      console.log("id:");
+      state.idOfWaterIntake = data.find(
+        item => item.created_at.split(" ")[0] === date
+      ).id;
+      console.log(state.idOfWaterIntake);
+    },
+    updateSumWaterIntakeOneDay(state, num) {
+      state.sumWaterIntakeOneDay += num;
     }
   },
   actions: {
@@ -13,13 +33,31 @@ export default {
       try {
         const response = await API.post("/water/sum", data);
         commit("sumWaterIntake", response.data.data);
+        commit("initSumWaterIntakeOneDay");
       } catch (error) {
         console.log(error);
       }
     },
-    async updateSumWaterIntake({ dispatch }, data) {
+    async fetchUserAllRecordingsOfWaterIntake({ commit }, { user_id, date }) {
+      try {
+        const response = await API.get(`/water/${user_id}`);
+        commit("idOfWaterIntake", { data: response.data.data, date });
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+    async addNewWaterIntake({}, data) {
       try {
         await API.post("/userWater", data);
+      } catch (error) {
+        console.log(error.response);
+      }
+    },
+    async updateWaterIntake({}, { water_id, data }) {
+      console.log(water_id);
+      console.log(data);
+      try {
+        await API.post(`/userWater/${water_id}`, data);
       } catch (error) {
         console.log(error.response);
       }
@@ -30,7 +68,10 @@ export default {
       return state.sumWaterIntake;
     },
     sumWaterIntakeOneDay(state) {
-      return state.sumWaterIntake.length ? +state.sumWaterIntake[0].sum : 0;
+      return state.sumWaterIntakeOneDay;
+    },
+    idOfWaterIntake(state) {
+      return state.idOfWaterIntake;
     }
   }
 };
