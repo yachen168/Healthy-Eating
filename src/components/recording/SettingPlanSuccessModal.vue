7b6f4d7ea3c984ec8be0f8f4dae6d611b474be8d
yachen <io260188@gmail.com>
2020-10-22 13:03:27 +0800

    fix: 飲食規劃完成彈跳視窗排版

--- src/components/recording/SettingPlanSuccessModal.vue
+++ src/components/recording/SettingPlanSuccessModal.vue
@@ -41,7 +41,11 @@ export default {
   position: fixed;
   width: 100%;
   height: 100%;
+  max-width: 576px;
   bottom: 0;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
   background-color: #f5f5f5;
   padding: 0 30px 42px;
 }
